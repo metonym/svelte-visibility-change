bfe16af3831bfaa4eb61a16b5d722a88435e7825
Eric Liu <ericyl.us@gmail.com>
2022-07-18 06:52:42 -0700

    chore: run prettier

--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import preprocess from "svelte-preprocess"
+import preprocess from "svelte-preprocess";
 
 export default defineConfig({
   plugins: [
