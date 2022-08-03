27bad54530028fd0605b8df2c918afc8884baea3
metonym <ericyl.us@gmail.com>
2022-08-03 11:06:20 -0700

    chore(vitest): simplify config

--- vitest.config.js
+++ vitest.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import preprocess from "svelte-preprocess";
 
-export default defineConfig({
+/** @type {import('vite').UserConfig} */
+export default {
   plugins: [
     svelte({
       preprocess: [preprocess()],
-      hot: !process.env.VITEST,
+      hot: false,
     }),
   ],
   test: {
-    globals: true,
     environment: "jsdom",
   },
-});
+};
