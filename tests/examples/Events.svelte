fc07c914beaf84b599e870d9547d0cc600b35dc6
Eric Liu <ericyl.us@gmail.com>
2025-03-21 17:45:39 -0700

    chore: replace `svelte-readme` with custom Vite app

---
+++ tests/examples/Events.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import VisibilityChange from "svelte-visibility-change";
+
+  let events: string[] = [];
+</script>
+
+<VisibilityChange
+  on:visible={() => (events = [...events, "on:visible"])}
+  on:hidden={() => (events = [...events, "on:hidden"])}
+/>
+
+{events.join(", ")}
